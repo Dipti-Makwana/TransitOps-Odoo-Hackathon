101fa3f0dab50a3c9e4cae9bbfe41c288a178485
Dipti-Makwana <dipti3dipti@gmail.com>
2026-07-12 09:49:43 +0530

    Add Vehicle and Driver API routes with POST endpoints

--- app.py
+++ app.py
@@ -1,5 +1,5 @@
 from flask import Flask
-from models import db
+from models import db, Vehicle, Driver, Trip
 
 app = Flask(__name__)
 app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///database.db'
@@ -11,6 +11,36 @@ with app.app_context():
 @app.route('/')
 def home():
     return "TransitOps backend is running!"
+  
+from flask import request, jsonify
+from datetime import datetime
+
+# ---------- ADD VEHICLE ----------
+@app.route('/vehicles', methods=['POST'])
+def add_vehicle():
+    data = request.json
+    vehicle = Vehicle(
+        registration_number=data['registration_number'],
+        model=data['model'],
+        type=data['type'],
+        max_load_capacity=data['max_load_capacity']
+    )
+    db.session.add(vehicle)
+    db.session.commit()
+    return jsonify({"message": "Vehicle added", "id": vehicle.id}), 201
+
+# ---------- ADD DRIVER ----------
+@app.route('/drivers', methods=['POST'])
+def add_driver():
+    data = request.json
+    driver = Driver(
+        name=data['name'],
+        license_number=data['license_number'],
+        license_expiry_date=datetime.strptime(data['license_expiry_date'], '%Y-%m-%d')
+    )
+    db.session.add(driver)
+    db.session.commit()
+    return jsonify({"message": "Driver added", "id": driver.id}), 201
 
 if __name__ == '__main__':
     app.run(debug=True)
